4824309657e3e6aa3ea6c1374d14aaacd4b9cccc
Lucas de Lima <139277469+Lucas-dev23@users.noreply.github.com>
2026-08-20 16:51:03 -0300

    Alteração no campo de resultado: "Novo valor integral"

--- src/services/calculoDependencia.js
+++ src/services/calculoDependencia.js
@@ -105,7 +105,7 @@ function calcularDependencia(form) {
         "Valor da(s) disciplina(s) em depenência: " + moeda(parcelaDP);
 
     document.getElementById("resNovaMensalidade").innerText =
-        "Nova Mensalidade sem Desconto: " + moeda(novaMensalidadeSemDesconto);
+        "Novo valor integral: " + moeda(novaMensalidadeSemDesconto);
 
     document.getElementById("resSemDesconto").innerText =
         "Mensalidade com desconto: " + moeda(novaMensalidade);
